0c69ee7b97e0c2e0a3fec041846faa4ad283ad05
DanielMeier12 <meier.da12@gmail.com>
2022-05-22 15:00:35 +0200

    add: L08/script

--- Aufgaben/L08/script.js
+++ Aufgaben/L08/script.js
@@ -1,1 +1,24 @@
+var sound = [
+    new Audio('assets/c.mp3'),
+    new Audio('assets/d.mp3'),
+    new Audio('assets/e.mp3'),
+    new Audio('assets/f.mp3'),
+    new Audio('assets/g.mp3'),
+    new Audio('assets/a.mp3'),
+    new Audio('assets/b.mp3'),
+    new Audio('assets/df.mp3'),
+    new Audio('assets/ef.mp3'),
+    new Audio('assets/gf.mp3'),
+    new Audio('assets/af.mp3'),
+    new Audio('assets/bf.mp3'),
+];
+window.addEventListener("load", function () {
+    document.querySelector(".play").addEventListener('click', playSample);
+});
+function playSample(sound) {
+    sound.play();
+}
+setInterval(function () {
+    // Anweisungen 
+}, 500);
 //# sourceMappingURL=script.js.map
---
+++ Aufgaben/L08/script.ts
@@ -0,0 +1,23 @@
+var sound:HTMLAudioElement[] =[
+    new Audio("./assets/c.mp3"),
+    new Audio('./assets/d.mp3'),
+    new Audio('./assets/e.mp3'),
+    new Audio('./assets/f.mp3'),
+    new Audio('./assets/g.mp3'),
+    new Audio('./assets/a.mp3'),
+    new Audio('./assets/b.mp3'),
+    new Audio('./assets/df.mp3'),
+    new Audio('./assets/ef.mp3'),
+    new Audio('./assets/gf.mp3'),
+    new Audio('./assets/af.mp3'),
+    new Audio('./assets/bf.mp3'),
+];
+function playSample(sound:HTMLAudioElement){
+    sound.play();
+}
+window.addEventListener("load", function(){
+    document.querySelector("#play").addEventListener('click',playSample);
+    })
+
+setInterval(function playSample() { 
+ }, 500);
