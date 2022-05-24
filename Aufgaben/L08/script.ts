214001f62b68273c554f4829a88df5f981698708
DanielMeier12 <meier.da12@gmail.com>
2022-05-24 15:43:47 +0200

    Update script.ts

--- Aufgaben/L08/script.ts
+++ Aufgaben/L08/script.ts
@@ -1,32 +1,32 @@
 var sound: string[]=[("./assets/c.mp3"),("./assets/d.mp3"),("./assets/e.mp3"),("./assets/f.mp3"),("./assets/g.mp3"),("./assets/a.mp3"),("./assets/b.mp3"),("./assets/df.mp3"),("./assets/ef.mp3"),("./assets/gf.mp3"),("./assets/af.mp3"),("./assets/bf.mp3")];
 var melodie: string[]=[sound[1],sound[2],sound[3],sound[4],sound[5],sound[4],sound[3],sound[2]]
-var zaehler: number= 0;
+var counter: number= 0;
 
-function playSample(soundQuelle: string): void{
-    var sound: HTMLAudioElement = new Audio(soundQuelle);
+function playSample(ausgabe: string): void{
+    var sound: HTMLAudioElement = new Audio(ausgabe);
     sound.play();
 }
 function playMelodie(): void{
     setInterval(function(): void{
-        playSample(melodie[zaehler]);
-        zaehler ++;
-        if (zaehler === 8) {
-            zaehler = 0;
+        playSample(melodie[counter]);
+        counter ++;
+        if (counter === 8) {
+            counter = 0;
         }
     }, 500);
 }
 window.addEventListener('load',function keyboard(): void {
-    document.querySelector("#c").addEventListener("click",function(): void{playSample(sound[1]);});
-    document.querySelector("#d").addEventListener("click",function(): void{playSample(sound[2]);});
-    document.querySelector("#e").addEventListener("click",function(): void{playSample(sound[3]);});
-    document.querySelector("#f").addEventListener("click",function(): void{playSample(sound[4]);});
-    document.querySelector("#g").addEventListener("click",function(): void{playSample(sound[5]);});
-    document.querySelector("#a").addEventListener("click",function(): void{playSample(sound[6]);});
-    document.querySelector("#b").addEventListener("click",function(): void{playSample(sound[7]);});
-    document.querySelector("#df").addEventListener("click",function(): void{playSample(sound[8]);});
-    document.querySelector("#ef").addEventListener("click",function(): void{playSample(sound[9]);});
-    document.querySelector("#gf").addEventListener("click",function(): void{playSample(sound[10]);});
-    document.querySelector("#af").addEventListener("click",function(): void{playSample(sound[11]);});
-    document.querySelector("#bf").addEventListener("click",function(): void{playSample(sound[12]);});
-    document.querySelector("#play").addEventListener("click",function(): void{playMelodie();});
+    document.getElementById("c").addEventListener("click",function(): void{playSample(sound[1]);});
+    document.getElementById("d").addEventListener("click",function(): void{playSample(sound[2]);});
+    document.getElementById("e").addEventListener("click",function(): void{playSample(sound[3]);});
+    document.getElementById("f").addEventListener("click",function(): void{playSample(sound[4]);});
+    document.getElementById("g").addEventListener("click",function(): void{playSample(sound[5]);});
+    document.getElementById("a").addEventListener("click",function(): void{playSample(sound[6]);});
+    document.getElementById("b").addEventListener("click",function(): void{playSample(sound[7]);});
+    document.getElementById("df").addEventListener("click",function(): void{playSample(sound[8]);});
+    document.getElementById("ef").addEventListener("click",function(): void{playSample(sound[9]);});
+    document.getElementById("gf").addEventListener("click",function(): void{playSample(sound[10]);});
+    document.getElementById("af").addEventListener("click",function(): void{playSample(sound[11]);});
+    document.getElementById("bf").addEventListener("click",function(): void{playSample(sound[12]);});
+    document.getElementById("play").addEventListener("click",function(): void{playMelodie();});
 });
