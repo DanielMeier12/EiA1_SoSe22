b59c47e2e68c239e58d8a7b9a16cc985883ccd98
DanielMeier12 <meier.da12@gmail.com>
2022-05-24 14:33:37 +0200

    changes L08

--- Aufgaben/L08/script.js
+++ Aufgaben/L08/script.js
@@ -1,21 +1,20 @@
 var sound = [("./assets/c.mp3"), ("./assets/d.mp3"), ("./assets/e.mp3"), ("./assets/f.mp3"), ("./assets/g.mp3"), ("./assets/a.mp3"), ("./assets/b.mp3"), ("./assets/df.mp3"), ("./assets/ef.mp3"), ("./assets/gf.mp3"), ("./assets/af.mp3"), ("./assets/bf.mp3")];
-var melodie = [sound['01'], sound['02'], sound['03'], sound['04'], sound['05'], sound['04'], sound['03'], sound['02'], sound['01']];
+var melodie = [sound[1], sound[2], sound[3], sound[4], sound[5], sound[4], sound[3], sound[2]];
 var zaehler = 0;
-window.addEventListener("load", keyboard);
 function playSample(soundQuelle) {
     var sound = new Audio(soundQuelle);
     sound.play();
 }
-function playBeat() {
+function playMelodie() {
     setInterval(function () {
         playSample(melodie[zaehler]);
         zaehler++;
-        if (zaehler === 3) {
+        if (zaehler === 8) {
             zaehler = 0;
         }
     }, 500);
 }
-function keyboard() {
+window.addEventListener('load', function keyboard() {
     document.querySelector("#c").addEventListener("click", function () { playSample(sound[1]); });
     document.querySelector("#d").addEventListener("click", function () { playSample(sound[2]); });
     document.querySelector("#e").addEventListener("click", function () { playSample(sound[3]); });
@@ -28,6 +27,6 @@ function keyboard() {
     document.querySelector("#gf").addEventListener("click", function () { playSample(sound[10]); });
     document.querySelector("#af").addEventListener("click", function () { playSample(sound[11]); });
     document.querySelector("#bf").addEventListener("click", function () { playSample(sound[12]); });
-    document.querySelector("#play").addEventListener("click", function () { playBeat(); });
-}
+    document.querySelector("#play").addEventListener("click", function () { playMelodie(); });
+});
 //# sourceMappingURL=script.js.map
--- Aufgaben/L08/script.ts
+++ Aufgaben/L08/script.ts
@@ -1,33 +1,32 @@
 var sound: string[]=[("./assets/c.mp3"),("./assets/d.mp3"),("./assets/e.mp3"),("./assets/f.mp3"),("./assets/g.mp3"),("./assets/a.mp3"),("./assets/b.mp3"),("./assets/df.mp3"),("./assets/ef.mp3"),("./assets/gf.mp3"),("./assets/af.mp3"),("./assets/bf.mp3")];
-var melodie: string[]=[sound['01'],sound['02'],sound['03'],sound['04'],sound['05'],sound['04'],sound['03'],sound['02'],sound['01']]
+var melodie: string[]=[sound[1],sound[2],sound[3],sound[4],sound[5],sound[4],sound[3],sound[2]]
 var zaehler: number= 0;
 
-window.addEventListener("load", keyboard);
-function playSample(soundQuelle: string): void {
+function playSample(soundQuelle: string): void{
     var sound: HTMLAudioElement = new Audio(soundQuelle);
     sound.play();
 }
-function playBeat(): void {
-    setInterval(function (): void {
-        playSample (melodie[zaehler]);
+function playMelodie(): void{
+    setInterval(function(): void{
+        playSample(melodie[zaehler]);
         zaehler ++;
-        if (zaehler === 3) {
+        if (zaehler === 8) {
             zaehler = 0;
         }
     }, 500);
 }
-function keyboard(): void {
-document.querySelector("#c").addEventListener("click", function(): void {playSample(sound[1]);});
-document.querySelector("#d").addEventListener("click", function(): void {playSample(sound[2]);});
-document.querySelector("#e").addEventListener("click", function(): void {playSample(sound[3]);});
-document.querySelector("#f").addEventListener("click", function(): void {playSample(sound[4]);});
-document.querySelector("#g").addEventListener("click", function(): void {playSample(sound[5]);});
-document.querySelector("#a").addEventListener("click", function(): void {playSample(sound[6]);});
-document.querySelector("#b").addEventListener("click", function(): void {playSample(sound[7]);});
-document.querySelector("#df").addEventListener("click", function(): void {playSample(sound[8]);});
-document.querySelector("#ef").addEventListener("click", function(): void {playSample(sound[9]);});
-document.querySelector("#gf").addEventListener("click", function(): void {playSample(sound[10]);});
-document.querySelector("#af").addEventListener("click", function(): void {playSample(sound[11]);});
-document.querySelector("#bf").addEventListener("click", function(): void {playSample(sound[12]);});
-document.querySelector("#play").addEventListener("click", function (): void { playBeat(); });
-  }
+window.addEventListener('load',function keyboard(): void {
+    document.querySelector("#c").addEventListener("click",function(): void{playSample(sound[1]);});
+    document.querySelector("#d").addEventListener("click",function(): void{playSample(sound[2]);});
+    document.querySelector("#e").addEventListener("click",function(): void{playSample(sound[3]);});
+    document.querySelector("#f").addEventListener("click",function(): void{playSample(sound[4]);});
+    document.querySelector("#g").addEventListener("click",function(): void{playSample(sound[5]);});
+    document.querySelector("#a").addEventListener("click",function(): void{playSample(sound[6]);});
+    document.querySelector("#b").addEventListener("click",function(): void{playSample(sound[7]);});
+    document.querySelector("#df").addEventListener("click",function(): void{playSample(sound[8]);});
+    document.querySelector("#ef").addEventListener("click",function(): void{playSample(sound[9]);});
+    document.querySelector("#gf").addEventListener("click",function(): void{playSample(sound[10]);});
+    document.querySelector("#af").addEventListener("click",function(): void{playSample(sound[11]);});
+    document.querySelector("#bf").addEventListener("click",function(): void{playSample(sound[12]);});
+    document.querySelector("#play").addEventListener("click",function(): void{playMelodie();});
+});
